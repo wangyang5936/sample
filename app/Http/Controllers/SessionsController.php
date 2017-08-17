2e95a5e6b4098c38f4280718f7e702890160662f
wangyang5936 <59369025@qq.com>
2017-08-17 08:54:48 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create'],
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -29,7 +36,7 @@ class SessionsController extends Controller
         {
             session()->flash('success', '欢迎回来！');
 
-            return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         }
         else
         {
